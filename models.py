5230078826a2bf121d88adb6c85ed19f6e50f42d
Ssak4395 <58580601+Ssak4395@users.noreply.github.com>
2019-12-06 14:36:11 +1100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# class Note(models.Model):
+#     title = models.CharField(max_length=32, default='Title'),
+#     content = models.TextField(null=True)
+#     user_id = models.IntegerField()
